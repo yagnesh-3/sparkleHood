5831271652e3c00cf438d302e29ac0f9a25275f6
Yagnesh <yagnesh.glk@gmail.com>
2025-04-26 15:10:32 +0530

    server changed

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
 import connectDB from "./config/db";
 import app from "./app";
 import dotenv from 'dotenv';
-import serverless from 'serverless-http';
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-// connectDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server Running in port ${PORT}`)
-//     })
-// })
-
-connectDB();
-
-module.exports = serverless(app);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server Running in port ${PORT}`)
+    })
+})
