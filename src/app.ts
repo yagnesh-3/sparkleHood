4940c14a0ebaf935956eec00d09f242537eac444
Yagnesh <yagnesh.glk@gmail.com>
2025-05-04 10:22:08 +0530

    public dir updated

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,18 @@ import incidentRoute from './routes/incident.route';
 import errorHandler from './middleware/errorHandler';
 import docs from './routes/api-docs.routes';
 import helmet from 'helmet';
-import limiter from './rateLimiter'
+import limiter from './rateLimiter';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(limiter);
 app.use(helmet());
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: './public' });
+  const file = readFileSync(join(__dirname, '../public/index.html'), 'utf8');
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(file);
 })
 app.get("/test", (req, res) => {
   throw new Error('Testing the error handler!');
